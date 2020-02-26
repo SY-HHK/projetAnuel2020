945deda32bc45ff7c5edf7dc26fc56f337669650
Louis <31516181+SY-HHK@users.noreply.github.com>
2020-02-26 09:43:19 +0100

    delete useless file

--- cPart/dataBaseMerger/include/database.h
+++
@@ -1,9 +0,0 @@
-#ifndef DATABASE_HEADER
-#define DATABASE_HEADER
-
-void get_conf_file(char host[], char dbuser[], char dbpass[], char dbname[]);
-void finish_with_err(MYSQL *conn);
-void db_connect(MYSQL *conn);
-void closeConnection(MYSQL *conn);
-
-#endif
