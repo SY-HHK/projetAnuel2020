624a46d6fcf89c2b89e2f3b6da4b014dd5cf78e3
Louis <31516181+SY-HHK@users.noreply.github.com>
2020-02-26 09:43:31 +0100

    delete useless file

--- cPart/dataBaseMerger/include/provider.h
+++
@@ -1,7 +0,0 @@
-#ifndef FRIDGE_HEADER
-#define FRIDGE_HEADER
-
-Provider initProvider(char *fName, char *lName, int phone, char *email, char *pwd, char *addr, char *companyName, float rate, int annul );
-ProviderList listProvider(MYSQL *conn);
-
-#endif
