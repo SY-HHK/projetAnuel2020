dcebdc19a76b4d83ff84a7eb0df884ec27e32c9c
Louis <31516181+SY-HHK@users.noreply.github.com>
2020-05-04 13:21:49 +0200

    vrai dump au bon format

--- cPart/dataBaseMerger/scripts/dump.sh
+++ cPart/dataBaseMerger/scripts/dump.sh
@@ -1,4 +1,16 @@
-MYSQL_PWD="test123" mysqldump --compact --insert-ignore --skip-triggers --no-create-db --no-create-info -u admin bringmeC > /home/hhk/Documents/projetAnuel2020/cPart/dataBaseMerger/scripts/script.sql
+hote_db="localhost"
+nom_bd="bringmeC"
+login_db="admin"
+pass_bd="test123"
+
+
+col="companyName, providerFirstName, providerLastName, providerBirth, providerEmail, providerPhone, providerAddress, cityName, cityDepartment, cityRegion, providerGuid"
+nom_table="PROVIDER INNER JOIN CITY ON PROVIDER.idCity = CITY.idCity"
+
+rm "/home/hhk/Documents/projetAnuel2020/cPart/dataBaseMerger/merger/cmake-build-debug/backup.mdb"
+
+mysql -h"$hote_db" -D"$nom_bd" -u "$login_db" -p"$pass_bd" -e "SELECT $col FROM $nom_table INTO OUTFILE '/var/www/html/backup.mdb' FIELDS TERMINATED BY ',';"
+
+echo "Export terminé"
 
 scp -i mdpScp script.sql user@51.75.133.145:~
-
